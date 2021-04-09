ced428c270793d737408851f02931495100b318e
Mohannad Khirallah <mkhirallah@KloudBusiness.com>
2021-04-09 22:08:49 +0300

    broken our home page into smaler compnents

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './../../styles/homepage.style.scss';
+import Direcotry from './../../components/directory/directory.component';
+
+const HomePage =() =>(
+
+    <div className='homepage'>
+        <Direcotry></Direcotry>
+    </div>
+);
+export default HomePage;
