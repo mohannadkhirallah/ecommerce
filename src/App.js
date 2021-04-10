a7f17ab3f1bcd02d75f1cc202429f5efbe2f4862
Mohannad Khirallah <mkhirallah@KloudBusiness.com>
2021-04-10 21:19:05 +0300

    adding header with shop page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import './App.css';
 import HomePage from './pages/homepage/homepage'
 import {Switch,Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-const HatsPage = () =>(
-  <div>
-    <h1>HATS Page</h1>
-  </div>
-)
+
 
 function App() {
   return (
     <div>
+    <Header/>
      <Switch>
        <Route exact path='/' component={HomePage}/>
        <Route path='/shop' component={ShopPage}/>
