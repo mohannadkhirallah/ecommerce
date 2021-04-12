f20edcd2202e5978b0feaac819bfe08fbd4757a0
Mohannad Khirallah <mkhirallah@KloudBusiness.com>
2021-04-13 00:11:01 +0300

    implemented fire base utils

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignOutPage from './pages/sign-in-up/sign-in-up.component';
 import React from 'react';
-import {auth} from '../src/firebase/firebase.utils';
+import {auth,createUserProfileDocument} from '../src/firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -23,9 +23,23 @@ class App extends React.Component {
 
   componentDidMount()
   {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth)
+      {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, ()=> console.log(this.state))
+        });
+      }
+      else{
+        this.setState({currentUser:userAuth})
+      }
+
     })
   }
 
--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SignIn from '../sign-in/sign-in.component';
+import Signup from '../../components/sign-up/sign-up.component';
 import './sign-in-up.style.scss';
 
 const SignInSignOutPage =() =>(
+
     <div className='sign-in-and-sign-up'>
-   <SignIn></SignIn>
+
+   <SignIn/>
+   <Signup/>
     </div>
 )
 
